61e215f88293b20b2c4f79f7223d8d8db09b28ca
Travis Mendez <Travis.mendez779@gmail.com>
2022-08-18 15:40:40 -0700

    commit tree 1d74d1fa1893ec41225a39ba1c2368400fe789a5

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Code your solution in this file!
+const hq = 42;
+const feet = 264;
+function distanceFromHqInBlocks(blocks) {
+    if (blocks > 42){
+        return blocks - hq;
+    }else if (blocks < 42){
+        return hq -blocks;
+    }
+    }
+    
+function distanceFromHqInFeet(blocks){
+return distanceFromHqInBlocks(blocks) * feet;
+
+}
+
+function distanceTravelledInFeet(hq, num) {
+
+    if (num > hq) { 
+        return (num - hq)*feet;
+    }else {
+        return (hq - num)*feet;
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let calculatesFarePrice;
+    let calculatesFeet= distanceTravelledInFeet(start, destination);
+   if (calculatesFeet < 400 ){
+        return calculatesFarePrice = 0;
+   } else if (calculatesFeet > 400 && calculatesFeet < 2000){
+       calculatesFarePrice = calculatesFeet - 400;
+       return (calculatesFarePrice * 0.02);
+   }
+   else if (calculatesFeet > 2000 && calculatesFeet < 2499){
+       calculatesFarePrice = 25;
+       return calculatesFarePrice;
+   }
+   else if (calculatesFeet > 2500){
+       return 'cannot travel that far';
+   }
+// last function was by Hieu Nguyen @ from Slack Aug 31st 2021 
+}
+
